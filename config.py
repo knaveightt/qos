7ba957c1ed44c194e08cf4f8e098f5e8f216ac30
John Osinski <johnosinski80@gmail.com>
2024-06-12 22:57:23 -0400

    added work-in-progress qtile config

---
+++ config.py
@@ -0,0 +1,253 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from libqtile import bar, layout, qtile, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    # Window Switching, Movement, Sizing
+    Key([mod], "j", lazy.layout.next(), desc="Move focus to next window"),
+    Key([mod], "k", lazy.layout.previous(), desc="Move focus to prev window"),
+    Key([mod], "h", lazy.layout.shrink_main(), desc="Decrease Master Size"),
+    Key([mod], "l", lazy.layout.grow_main(), desc="Increase Master Size"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod], "g", lazy.layout.swap_main(), desc="Increase Master Size"),
+    Key([mod], "r", lazy.layout.reset(), desc="Decrease Master Size"),    
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "z", lazy.screen.toggle_group(), desc="Focus on prior group"),
+    
+    # Layout Switching
+    Key([mod], "t", lazy.to_layout_index(0), desc="Increase Master Size"),
+    Key([mod], "d", lazy.to_layout_index(1), desc="Increase Master Size"),
+    Key([mod], "c", lazy.to_layout_index(2), desc="Increase Master Size"),
+    Key([mod], "v", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
+    Key([mod], "b", lazy.hide_show_bar(), desc="Increase Master Size"),
+
+    # TreeTab Layout Navigation
+    Key([mod], "o", lazy.layout.move_right(), desc="Increase Master Size"),
+    Key([mod], "i", lazy.layout.move_left(), desc="Increase Master Size"),
+    Key([mod], "y", lazy.layout.move_down(), desc="Increase Master Size"),
+    Key([mod], "u", lazy.layout.move_up(), desc="Increase Master Size"),
+    Key([mod, "shift"], "i", lazy.layout.collapse_branch(), desc="Increase Master Size"),
+    Key([mod, "shift"], "o", lazy.layout.expand_branch(), desc="Increase Master Size"),
+    Key([mod, "shift"], "u", lazy.layout.section_up(), desc="Increase Master Size"),
+    Key([mod, "shift"], "y", lazy.layout.section_down(), desc="Increase Master Size"),
+
+    # Launchers and Applications
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "space", lazy.spawn(terminal), desc="Launch Emacs"),
+    Key([mod], "p", lazy.spawn(terminal), desc="Launch Program Launcher"),
+    Key([mod, "shift"], "p", lazy.spawn(terminal), desc="Launch Command Launcher"),
+    Key([mod], "e", lazy.spawn(terminal), desc="Launch Terminal File Explorer"),
+    Key([mod], "w", lazy.spawn(terminal), desc="Launch Window Switcher"),
+    Key([mod, "shift"], "q", lazy.spawn(terminal), desc="Launch X Kill"),
+    Key([mod, "shift"], "z", lazy.spawn(terminal), desc="Lock Screen"),
+
+    
+    Key([mod], "x", lazy.spawn(terminal), desc="Launch Extended Prompt"),
+
+    # ScratchPad
+    Key([mod], "s", lazy.group["scratchterm"].dropdown_toggle("term"), desc="Launch Extended Prompt"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+    # Control Keybinds
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "control"], "p", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+]
+
+# Custom TreeTab Control Keybinds
+def prompt_get_notify(qtile):
+    try:
+        mb = qtile.widgets_map["prompt"]
+        mb.start_input("Echo", notif, None)
+    except:
+        mb = None
+
+def notif(args):
+    qtile.cmd_spawn('dunstify "%s"' % args)
+
+#keys.append(Key([mod], "z", lazy.function(prompt_get_notify)))
+
+def sectionadd(args):
+    qtile.current_layout.cmd_add_section(args)
+
+def prompt_get_sectionadd(qtile):
+    try:
+        mb = qtile.widgets_map["prompt"]
+        mb.start_input("Section", sectionadd, None)
+    except:
+        mb = None
+
+keys.append(Key([mod, "control"], "o", lazy.function(prompt_get_sectionadd)))
+
+def sectiondel(args):
+    qtile.current_layout.cmd_del_section(args)
+
+def prompt_get_sectiondel(qtile):
+    try:
+        mb = qtile.widgets_map["prompt"]
+        mb.start_input("Section", sectiondel, None)
+    except:
+        mb = None
+
+keys.append(Key([mod, "control"], "i", lazy.function(prompt_get_sectiondel)))
+
+# Add key bindings to switch VTs in Wayland.
+# We can't check qtile.core.name in default config as it is loaded before qtile is started
+# We therefore defer the check until the key binding is run by using .when(func=...)
+for vt in range(1, 8):
+    keys.append(
+        Key(
+            ["control", "mod1"],
+            f"f{vt}",
+            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            desc=f"Switch to VT{vt}",
+        )
+    )
+
+groups = [
+    Group("1", layout="monadtall"),
+    Group("2", layout="monadtall"),
+    Group("3", layout="monadtall"),
+    Group("4", layout="monadtall"),
+    Group("5", layout="monadtall"),
+    ScratchPad("scratchterm",[DropDown("term","sakura -f 'JetBrainsMono Nerd Font 9'", x=0.12, y=0.02, width=0.75, height=0.6, on_focus_lost_hide=False)]),
+]
+
+from libqtile.dgroups import simple_key_binder
+dgroups_key_binder = simple_key_binder("mod4")
+
+layouts = [
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.MonadTall(),
+    layout.Max(),
+    layout.TreeTab(),
+
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="sans",
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
+            ],
+            24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+floats_kept_above = True
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
